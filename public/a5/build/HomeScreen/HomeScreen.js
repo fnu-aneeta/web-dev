dcc8c32e4797d4a38c3bfeedb123cbf32bd3ce20
aneeta <aneeta.f@northeastern.edu>
2021-10-14 21:45:20 -0400

    pushing a5

--- public/a5/build/HomeScreen/HomeScreen.js
+++ public/a5/build/HomeScreen/HomeScreen.js
@@ -7,7 +7,7 @@ import ExploreComponent from "../HomeScreen/ExploreComponent.js";
       <div class = "container">
         <div class="row mt-2">
            <div class="col-2 col-sm-2 col-md-2 col-lg-1 col-xl-2 col-xxl-2">
-                ${NavigationSidebar()}
+                ${NavigationSidebar('home')}
             </div>
             <div class="col-6 col-sm-10 col-md-10 col-lg-7 col-xl-6 col-xxl-6">
              ${HomeComponent()}
