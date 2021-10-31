f7884e5f388987572ae877e4f0c773191dec0f87
aneeta <aneeta.f@northeastern.edu>
2021-10-30 23:03:41 -0400

    pushing a6

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import HelloWorld from "./hello";
+//import todos from './components/todos.json';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+//import NavigationExample from "./components/NavigationExample.js";
+//import NavigationSidebar from "./components/a6/Build/NavigationSidebar";
+//import WhoToFollowListItem from "./components/a6/Build/WhoToFollowList/WhoToFollowListItem";
+//import WhoToFollowList from "./components/a6/Build/WhoToFollowList/WhoToFollowList";
+//import ExploreScreen from "./components/a6/Build/ExploreScreen/ExploreScreen";
+//import HomeScreen from "./components/a6/Build/HomeScreen/HomeScreen";
+//import ExploreScreen from "./components/a6/Build/ExploreScreen/ExploreScreen";
 
 ReactDOM.render(
-   <React.StrictMode>
-       <App />
-    </React.StrictMode>,
+    <App/>,
     document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
 
 
