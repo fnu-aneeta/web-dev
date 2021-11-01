8279a1dbf4c4b18e2026caf7a153bcbfa7b36c50
aneeta <aneeta.f@northeastern.edu>
2021-10-31 20:58:42 -0400

    pushing

--- public/a5/build/PostList/PostItem.js
+++ public/a5/build/PostList/PostItem.js
@@ -2,17 +2,17 @@ const PostItem = (posts) => {
     return(`
 <div class="row mt-1">
      <div class="col-1">
-    <img src=${posts.avatarIcon} class="rounded-circle img-fluid" width="55px"/>
+    <img src=${posts.avatarIcon} class="rounded-circle float-start" width="32"/>&nbsp;
     </div>
      <div class="col-11">
-       <div class="wd-name fw-bold">${posts.userName}
-       <i class="fas fa-check-circle text-detail-color">&nbsp;</i>
-        <span class="wd-author-handle fw-normal">${posts.handle} &middot; ${posts.time} 
-         <i class="fa fa-ellipsis-h float-end"></i></span>
+       <div class="wd-name fw-bold">${posts.userName}&nbsp;
+       <i class="fas fa-check-circle text-detail-color" style={"fontSize: 0.9em"}>&nbsp;</i>
+        <span class="wd-author-handle fw-normal">${posts.handle} &middot; &nbsp; ${posts.time} 
+         <i class="fa fa-ellipsis-h float-end"></i>
+        </span>
        </div>
-        <p class="wd-main-content">${posts.body}</p>
-    </div>
-</div>
+        <p class="wd-main-content">${posts.body}&nbsp; </p>
+    
      <div class = "card border-left border-right wd-card-rounded-border wd-color-card-body">
                     <img src = ${posts.linkImage}
                          class="card-img-top wd-card-image-border">
@@ -38,15 +38,19 @@ const PostItem = (posts) => {
                     <i class="fas fa-retweet"></i></a>
                 <span class="wd-numbers">${posts.retweets}</span></div>
             <div class="wd-icon-distant">
-                <a class="wd-text-link" href = "#">
+                <a class="wd-text-link d-sm-none d-md-inline-block d-lg-inline-block d-xl-inline-block" href = "#">
                     <i class="fa fa-heart wd-icon-heart" aria-hidden="true"></i></a>
                 <span class="wd-numbers">${posts.favs}</span></div>
             <div class="wd-icon-distant">
                 <a class="wd-text-link" href = "#">
-                    <i class="fas fa-upload"></i></a>
-                <span class="wd-numbers"></span></div>
+                    <i class="fas fa-upload"></i>
+                </a>
+                <span class="wd-numbers"></span>
+            </div>
                  
         </div>
+ </div>
+ </div>
     <br/>    
     <br/>    
       <hr> 
