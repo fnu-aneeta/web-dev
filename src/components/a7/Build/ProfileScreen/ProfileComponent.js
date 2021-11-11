6b336629bb4c75ef8f8bba7dbea6544e55d6d2bb
aneeta <aneeta.f@northeastern.edu>
2021-11-11 17:38:52 -0500

    pushing a7

--- src/components/a7/Build/ProfileScreen/ProfileComponent.js
+++ src/components/a7/Build/ProfileScreen/ProfileComponent.js
@@ -1,15 +1,9 @@
 import React from "react";
-// import PostList from "../PostList/PostList.js";
-// import TweetList from "../TweetList/TweetList";
-// import EditProfileList from "../Profile/ProfileList";
-import EditProfileItem from "../EditProfile/EditProfileItem";
-import EditProfileList from "../EditProfile/EditProfileList";
 import ProfileList from "../Profile/ProfileList";
 
 const ProfileComponent = () => {
     return(
         <>
-            <EditProfileList/>
             <ProfileList/>
         </>
     );
