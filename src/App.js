98b56e0d7944bdb5c718f1dc01d47c0b5c5594b2
aneeta <aneeta.f@northeastern.edu>
2021-11-11 00:39:38 -0500

    pushing a7

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
-import HelloWorld from "./components/a6/HelloWorld";
-import Practice from "./components/a6/Practice/index";
-import Build from "./components/a6/Build/index";
+import HelloWorld from "./components/a7/HelloWorld";
+import Practice from "./components/a7/Practice/index";
+import Build from "./components/a7/Build";
 import {BrowserRouter, Route} from "react-router-dom";
-import ExploreScreen from "./components/a6/Build/ExploreScreen/ExploreScreen";
-import HomeScreen from "./components/a6/Build/HomeScreen/HomeScreen";
-
+import ExploreScreen from "./components/a7/Build/ExploreScreen/ExploreScreen";
+import HomeScreen from "./components/a7/Build/HomeScreen/HomeScreen";
+import ProfileScreen from "./components/a7/Build/ProfileScreen/ProfileScreen";
+// import WhoToFollowList from "./components/a7/Build/WhoToFollowList/WhoToFollowList";
+import {combineReducers,createStore} from "redux";
+import {Provider} from "react-redux";
+import who from "./reducers/who";
+import tweets from "./reducers/tweets";
+import profile from "./reducers/profile";
+//
+// const store = createStore(who);
+const reducer = combineReducers({tweets: tweets, who, profile})
+const store = createStore(reducer);
 
 function App() {
   return (
        <BrowserRouter>
-        <div className="container">
+     <Provider store={store}>
 
-          <Route path="/a6/hello" exact={true}>
+         <div className="container">
+
+          <Route path="/a7/hello" exact={true}>
             <HelloWorld/>
           </Route>
-          <Route path={["/", "/a6", "/a6/practice"]} exact={true}>
+          <Route path={["/", "/a7", "/a7/practice"]} exact={true}>
             <Practice/>
           </Route>
-          <Route path="/a6/build" exact={true}>
+          <Route path="/a7/build">
             <Build/>
           </Route>
-            <Route path="/a6/twitter/home" exact={true} component={HomeScreen}/>
-            <Route path="/a6/twitter/explore" exact={true}
+            <Route path="/a7/twitter/home" exact={true} component={HomeScreen}/>
+            <Route path="/a7/twitter/explore" exact={true}
                    component={ExploreScreen}/>
+             <Route path="/a7/twitter/profile" exact={true}
+                    component={ProfileScreen}/>
 
         </div>
+           </Provider>
        </BrowserRouter>
 
   );
