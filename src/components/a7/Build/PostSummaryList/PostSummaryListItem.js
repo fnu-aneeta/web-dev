d35d9bbb43fe80637aad8ee47ad9a906895ec468
aneeta <aneeta.f@northeastern.edu>
2021-11-11 17:34:29 -0500

    pushing a7

--- src/components/a7/Build/PostSummaryList/PostSummaryListItem.js
+++ src/components/a7/Build/PostSummaryList/PostSummaryListItem.js
@@ -26,54 +26,55 @@ const PostSummaryListItem = (
 
                   {/*{JSON.stringify(post)}*/}
                             <div className="row">
-                                {/*<div className="col-10 wd-new-line">*/}
+                                <div className="col-10 wd-new-line">
 
-                                {/*    <div className={`fw-normal text-color ${post.topic ? '\n' : ''}`}>{post.topic}</div>*/}
-                                <div className="col-9 col-md-10">
-                                    <div className="text-muted">{post.topic}</div>
+                                    <div className={`fw-normal text-color ${post.topic ? '\n' : ''}`}>{post.topic}</div>
+                                {/*<div className="col-9 col-md-10">*/}
+                                {/*    <div className="text-muted">{post.topic}</div>*/}
 
-                                   {/*<span className="fw-bold text-detail-color">{post.userName}</span>*/}
+                                   <span className="fw-bold text-detail-color">{post.userName}</span>
 
-                                   {/* <span className = "fa-stack" style={{"fontSize": "0.8em"}}>*/}
-                                   {/*     <i className = "fas fa-check-circle fa-stack-1x fa-inverse">&nbsp;</i>*/}
-                                   {/* </span>*/}
-                                   {/* <h7 className = "text-color">{post.time}</h7>*/}
-                                   {/*     <br/>*/}
-                                    <div>
-                                      <span className="fw-bold">
-                                          {post.userName}
-                                          <span className="fa-stack" style={{"fontSize": "0.5em"}}>
-                                            <i className="fas fa-circle fa-stack-2x"></i>
-                                            <i className="fas fa-check fa-stack-1x fa-inverse"></i>
-                                          </span>
-                                      </span>
-                                        <span className="text-muted">
-                                            - {post.time}
-                                        </span>
-                                    </div>
-                                {/*    <h7 className="text-detail-color">{post.title}</h7>*/}
-                                {/*  <br/>*/}
-                                {/*    <h7 className="fw-normal text-color">{post.tweets}</h7>*/}
-                                {/*</div>*/}
-                                {/*<div className="col-2 mt-3">*/}
-                                {/*    <img className="rounded img-fluid" src={post.image}*/}
-                                {/*         width="75px"*/}
-                                {/*         height="75px"/>*/}
-                                {/*</div>*/}
-                                    <div className="fw-bold">{post.title}</div>
+                                    <span className = "fa-stack" style={{"fontSize": "0.8em"}}>
+                                        <i className = "fas fa-check-circle fa-stack-1x fa-inverse">&nbsp;</i>
+                                    </span>
+                                    <h7 className = "text-color">{post.time}</h7>
+                                        <br/>
+                                    {/*<div>*/}
+                                    {/*  <span className="fw-bold">*/}
+                                    {/*      {post.userName}*/}
+                                    {/*      <span className="fa-stack" style={{"fontSize": "0.5em"}}>*/}
+                                    {/*        <i className="fas fa-circle fa-stack-2x"></i>*/}
+                                    {/*        <i className="fas fa-check fa-stack-1x fa-inverse"></i>*/}
+                                    {/*      </span>*/}
+                                    {/*  </span>*/}
+                                    {/*    <span className="text-muted">*/}
+                                    {/*        - {post.time}*/}
+                                    {/*    </span>*/}
+                                    {/*</div>*/}
+                                    <h7 className="text-detail-color">{post.title}</h7>
+                                  <br/>
+                                    <h7 className="fw-normal text-color">{post.tweets}</h7>
                                 </div>
-                                <div className="col-3 col-md-2">
-                                    {/*<img src={post['logo-image']}*/}
-                                         <img src={post.image}
-                                         // alt="qwe"
-                                         width="75"
-                                         height="75"
-                                         className="float-end"
-                                         style={{"borderRadius" : "10px"}}/>
+
+                                <div className="col-2 mt-3">
+                                    <img className="rounded img-fluid" src={post.image}
+                                         width="75px"
+                                         height="75px"/>
                                 </div>
+                                    {/*<div className="fw-bold">{post.title}</div>*/}
+
+                            </div>
+                                {/*<div className="col-3 col-md-2 mt-3">*/}
+                                {/*         <img src={post.image}*/}
+                                {/*         // alt="qwe"*/}
+                                {/*         width="50"*/}
+                                {/*         height="50"*/}
+                                {/*         className="float-end"*/}
+                                {/*         style={{"borderRadius" : "10px"}}/>*/}
+                                {/*</div>*/}
 
                         
-                        </div>
+
               </li>
     );
 }
